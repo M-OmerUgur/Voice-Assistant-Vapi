8ac87ff378ad58ad02812e9938234472a26de2c0
M-OmerUgur <muguromer@gmail.com>
2025-05-09 16:09:56 +0300

    manifest logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#000000" />
+      </head>
       <body
         className={`${monaSans.className} antialiased pattern`}
       >
